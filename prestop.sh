d8130c61d5adae33fcbb3d8183dc691d949908b7
Nashrah31 <35250488+Nashrah31@users.noreply.github.com>
2021-01-09 02:20:40 +0700

    Update prestop.sh

--- prestop.sh
+++ prestop.sh
@@ -3,4 +3,4 @@
 #echo $id > test
 cd kafka_2.13-2.6.0
 #bin/kafka-consumer-groups.sh --bootstrap-server kafka.prod.svc.cluster.local:9092  --delete --group $(<test)
-bin/kafka-consumer-groups.sh --bootstrap-server kafka.prod.svc.cluster.local:9092  --delete --group @[$HOSTNAME]
+bin/kafka-consumer-groups.sh --bootstrap-server kafka.prod.svc.cluster.local:9092  --delete --group $HOSTNAME
